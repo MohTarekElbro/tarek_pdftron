fe32b449bcf6909da94143e9ce16a52c2ad87aaf
MohTarekElbro <52460292+MohTarekElbro@users.noreply.github.com>
2023-10-04 14:20:32 +0300

    fix screen shot

--- ios/Classes/DocumentViewFactory.m
+++ ios/Classes/DocumentViewFactory.m
@@ -20,6 +20,13 @@
 
 - (NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id)args
 {
+     let field = UITextField()
+    field.isSecureTextEntry = true
+    self.addSubview(field)
+    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    self.layer.superlayer?.addSublayer(field.layer)
+    field.layer.sublayers?.first?.addSublayer(self.layer)
     return [PdftronFlutterPlugin registerWithFrame:frame viewIdentifier:viewId messenger:self.messenger];
 }
 
